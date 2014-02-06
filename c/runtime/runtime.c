0c0dbec41fe6bacc6bc2a2afd89cb3e7ba8ebc0f
Jakub Wasilewski <wasyl@wasyl.eu>
2014-02-06 17:19:59 +0100

    Version number bump: 0.1-ag

--- c/runtime/runtime.c
+++ c/runtime/runtime.c
@@ -17,7 +17,7 @@
 //  Version
 // ===============================================================
 
-#define SEPTEMBER_VERSION "0.1-affirmation"
+#define SEPTEMBER_VERSION "0.1-agility"
 
 // ===============================================================
 //  Prototype objects
