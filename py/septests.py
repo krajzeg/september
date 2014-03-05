6d56d1b2236848a72a96b8db62b3f75a6897376d
Jakub Wasilewski <wasyl@wasyl.eu>
2014-03-05 18:51:00 +0100

    Test runner now supports color output using ANSI.

--- py/septests.py
+++ py/septests.py
@@ -28,6 +28,12 @@ class TestResult:
         self.status = status
 
     def __str__(self):
+        if self.status == TestResult.SUCCESS:
+            return Ansi.in_color(self.text, Ansi.WHITE)
+        elif self.status == TestResult.FAILURE:
+            return Ansi.in_color(self.text, Ansi.RED)
+        elif self.status == TestResult.WARNING:
+            return Ansi.in_color(self.text, Ansi.YELLOW)
         return self.text
 
 
@@ -140,32 +146,60 @@ class TestFile:
                 return f.read()
 
 ##############################################
+# ANSI controls
+##############################################
+
+class Ansi:
+    BLACK, RED, GREEN, BROWN, BLUE, MAGENTA, CYAN, GRAY = range(8)
+    DARK_GRAY, LIGHT_RED, LIGHT_GREEN, YELLOW, LIGHT_BLUE, LIGHT_MAGENTA, \
+    LIGHT_CYAN, WHITE = range(8,16)
+
+    start = "\x1b["
+    enabled = True
+
+    @classmethod
+    def sequence(cls, text):
+        return cls.start + text if cls.enabled else ""
+
+    @classmethod
+    def clear_screen(cls):
+        return cls.sequence("2J")
+
+    @classmethod
+    def fg(cls, which):
+        string = cls.reset()
+        if which > 8:
+            string += cls.sequence("1m")
+            which -= 8
+        string += cls.sequence("3%sm" % which)
+        return string
+
+    @classmethod
+    def bg(cls, which):
+        return cls.sequence("4%sm" % which)
+
+    @classmethod
+    def reset(cls):
+        return cls.sequence("0m")
+
+    @classmethod
+    def in_color(cls, text, fg = 7, bg = 0):
+        return cls.reset() + cls.fg(fg) + cls.bg(bg) + text + cls.reset()
+
+##############################################
 # Phases of execution
 ##############################################
 
-def parse_arguments(args):
-    """Parses the command line and returns the root directory with the test
-    files."""
-    if len(args) < 2:
-        sys.stderr.write("Usage:\n\tseptests.py <test directory or test "
-                         "file>\n")
-        sys.exit(2)
-
-    root_dir = args[1]
-    if not path.exists(root_dir) or not path.isdir(root_dir):
-        sys.stderr.write("Usage:\n\tseptests.py <test directory>\n")
-        sys.exit(2)
-
-    return root_dir
-
-
-def gather_test_files(root_directory):
+def gather_test_files(file_args):
     """Gather all the test files to be run based on the root directory passed
      by the user."""
-    for current_dir, _, files in os.walk(root_directory):
-        for file in filter(lambda f: f.endswith(".sep"), files):
-            yield TestFile(root_directory, path.join(current_dir, file))
-
+    for dir_or_file in file_args:
+        if path.isdir(dir_or_file):
+            for current_dir, _, files in os.walk(dir_or_file):
+                for file in filter(lambda f: f.endswith(".sep"), files):
+                    yield TestFile(dir_or_file, path.join(current_dir, file))
+        else:
+            yield TestFile(path.dirname(dir_or_file), dir_or_file)
 
 def clean_leftovers(tests):
     """Cleans left-over files from previous executions."""
@@ -177,7 +211,8 @@ def ask_for_blessing(test):
     """Asks the user to bless the output of the test, and blesses it if the
     user chooses so.
     """
-    print("Is this output correct ([Y]es/[N]o/[S]top)? ", end="")
+    msg = "Is this output correct ([Y]es/[N]o/[S]top)? "
+    print(Ansi.in_color(msg, Ansi.WHITE), end="")
     choice = input().lower()
     if choice.startswith("y"):
         test.bless_output()
@@ -189,10 +224,9 @@ def ask_for_blessing(test):
 
 def check_new_test(test):
     """Asks the user to bless a new test."""
-    print()
-    print("#" * 79)
-    print("Test '%s' was run for the first time with the following output:\n" %
-          test.name)
+    msg = "Test '%s' was run for the first time with the following output:\n"
+    print(Ansi.clear_screen())
+    print(Ansi.in_color(msg % test.name, Ansi.WHITE))
 
     for line in test.output.split("\n"):
         print("  ", line)
@@ -201,10 +235,9 @@ def check_new_test(test):
 
 def check_bad_test(test):
     """Asks the user to bless a test that gave a new output."""
-    print()
-    print("#" * 79)
-    print("Test '%s' gave output different than expected:" %
-          test.name)
+    msg = "Test '%s' gave output different than expected:"
+    print(Ansi.clear_screen())
+    print(Ansi.in_color(msg % test.name, Ansi.WHITE))
     print("Lines marked with 'E' are from expected output.")
     print("Lines marked with 'A' are from actual output.")
 
@@ -218,16 +251,16 @@ def check_bad_test(test):
 
     for expected_line, actual_line in zip(expected, actual):
         if expected_line == actual_line:
-            print("  ", expected_line)
+            print("  %s" % expected_line)
         else:
-            print("E ", expected_line)
-            print("A ", actual_line)
+            print(Ansi.in_color("E %s" % expected_line, Ansi.GREEN))
+            print(Ansi.in_color("A %s" % actual_line, Ansi.RED))
 
     # ask for blessing
     ask_for_blessing(test)
 
 def run_tests(tests, distribution_root):
-    print("Running %d tests:" % len(tests))
+    print(Ansi.in_color("Running %d tests:" % len(tests), Ansi.WHITE))
 
     max_width = max(len(test.name) for test in tests)
     test_results = {}
@@ -250,14 +283,15 @@ def run_tests(tests, distribution_root):
     # final result
     failed_tests = [t for t in tests if t.failed]
     if failed_tests:
-        print("\n%d of %d tests failed." % (len(failed_tests), len(tests)))
+        fail_str = "\n%d of %d tests failed." % (len(failed_tests), len(tests))
+        print(Ansi.in_color(fail_str, Ansi.LIGHT_RED))
         for test in failed_tests:
             print("  %s" % test.name, end="")
             print("." * (max_width + 2 - len(test.name)), end="")
             print(" %s" % test.result)
         return 1
     else:
-        print("\nAll tests green.")
+        print(Ansi.in_color("\nAll tests passed.", Ansi.WHITE))
         return 0
 
 ##############################################
@@ -265,13 +299,31 @@ def run_tests(tests, distribution_root):
 ##############################################
 
 if __name__ == "__main__":
+    import optparse
+
+    # set up option parser
+    p = optparse.OptionParser("usage: septest.py [options] <test directories "
+                              "or files>")
+    p.add_option("-a", "--ansi",
+                     action="store_true", dest = "ansi", default=True,
+                     help="Enables color output using ANSI sequences.")
+    p.add_option("-n", "--no-ansi",
+                 action="store_false", dest = "ansi",
+                 help="Disables color output using ANSI sequences.")
+    options, args = p.parse_args(sys.argv)
+
+    # configure
+    if len(args) < 2:
+        p.print_usage()
+        sys.exit(2)
+    Ansi.enabled = options.ansi
+
     # determine the root of the September distribution
-    path_to_septests = path.dirname(sys.argv[0])
+    path_to_septests = path.dirname(args[0])
     distribution_root = path.abspath(path.join(path_to_septests, "../"))
 
-    # run tests from a chosen directory
-    root_dir = parse_arguments(sys.argv)
-    tests = list(gather_test_files(root_dir))
+    # run selected tests
+    tests = list(sorted(gather_test_files(args[1:]), key=lambda t: t.name))
     clean_leftovers(tests)
     result = run_tests(tests, distribution_root)
     sys.exit(result)
