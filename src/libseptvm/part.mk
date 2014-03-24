4f3dc1f47805b9d426789c8cc778b317563c6387
Jakub Wasilewski <wasyl@wasyl.eu>
2014-03-24 02:16:57 +0100

    Fixed libseptvm's part.mk so that the library is built correctly if it exists.

--- src/libseptvm/part.mk
+++ src/libseptvm/part.mk
@@ -31,6 +31,7 @@ LIBSVM_TARGET_LIB := $(LIB_DIR)/libseptvm.a
 libseptvm: $(LIBSVM_TARGET_LIB)
 
 $(LIBSVM_TARGET_LIB): $(LIBSVM_OBJECTS) | $(LIB_DIR)
+	-$(RM) $@
 	$(AR) rcs $@ $(LIBSVM_OBJECTS)
 
 # ==========================
