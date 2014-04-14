6768d7e02db3487592ff40f87ce99363810d75ad
Jakub Wasilewski <wasyl@wasyl.eu>
2014-04-14 21:30:33 +0200

    First implementation of writeable array indices.

--- src/runtime/arrayp.c
+++ src/runtime/arrayp.c
@@ -51,6 +51,51 @@ SepItem arrayiterator_next(SepObj *scope, ExecutionFrame *frame) {
 }
 
 // ===============================================================
+//  Array index slots
+// ===============================================================
+
+/**
+ * This type of slot represents one element of the array. The special slot
+ * has to be used to allow writing to array indices.
+ */
+typedef struct ArrayIndexSlot {
+	// the standard stuff every slot has
+	Slot base;
+	// the array from which this value came from
+	SepArray *array;
+	// the index from which it came
+	uint32_t index;
+} ArrayIndexSlot;
+
+// Retrieve a value from an array index.
+SepV _array_index_slot_retrieve(Slot *slot, SepV context) {
+	ArrayIndexSlot *this = (ArrayIndexSlot*)slot;
+	return array_get(this->array, this->index);
+}
+
+// Set a value inside an array element.
+SepV _array_index_slot_store(Slot *slot, SepV context, SepV new_value) {
+	ArrayIndexSlot *this = (ArrayIndexSlot*)slot;
+	return array_set(this->array, this->index, new_value);
+}
+
+// The v-table for the array index slot.
+SlotVTable array_index_slot_vt = {
+	&_array_index_slot_retrieve,
+	&_array_index_slot_store
+};
+
+// Creates a new "array index" slot capable of writing values to the array.
+ArrayIndexSlot *array_index_slot_create(SepArray *array, uint32_t index) {
+	// TODO: this really should be managed memory once we can manage slots
+	ArrayIndexSlot *slot = mem_unmanaged_allocate(sizeof(ArrayIndexSlot));
+	slot->base.vt = &array_index_slot_vt;
+	slot->array = array;
+	slot->index = index;
+	return slot;
+}
+
+// ===============================================================
 //  Indexing/slicing
 // ===============================================================
 
@@ -69,8 +114,13 @@ SepItem array_index(SepObj *scope, ExecutionFrame *frame) {
 		index = index_i;
 	}
 
-	// return the value
-	return item_rvalue(array_get(this, index));
+	// create the slot used for assignment to array indices
+	SepV value = array_get(this, index);
+		or_propagate(value);
+	Slot *slot = (Slot*)array_index_slot_create(this, index);
+
+	// return an l-value so assigning to indices works
+	return item_lvalue(slot, value);
 }
 
 // ===============================================================
