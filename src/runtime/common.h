34f214080521e41b6d3df1f2e7dcb9f7cbb5cc63
Jakub Wasilewski <wasyl@wasyl.eu>
2014-05-25 17:12:55 +0200

    Switched septvm.h include to relative paths to make sure makefile dependencies are correct.

---
+++ src/runtime/common.h
@@ -0,0 +1,18 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+/*****************************************************************
+ **
+ ** runtime/common.h
+ **
+ ** Common includes and definitions for the whole runtime.
+ **
+ ***************
+ ** September **
+ *****************************************************************/
+
+#include "../libseptvm/septvm.h"
+
+/*****************************************************************/
+
+#endif
